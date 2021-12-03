08282a5062b0679a1a323acdd1d68fe020de5ef9
Maxime Bonhomme <maxime@everpress.com>
2021-12-03 12:54:29 +0000

    infinite scroll

--- components/NFTGallery.js
+++ components/NFTGallery.js
@@ -1,7 +1,8 @@
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { updateNFTs, setStatus } from '../reducers/nfts';
+import { updateNFTs, setStatus, setNFTs } from '../reducers/nfts';
 
 import config from '../config';
 
@@ -35,11 +36,55 @@ const NFTGallery = () => {
     }
   };
 
+  const handleRefresh = async () => {
+    dispatch(setStatus('loading'));
+    try {
+      const opensea = await axios.get('/api/nfts', {
+        params: {
+          address,
+          limit: config.listLimit,
+          offset: 0
+        }
+      });
+
+      dispatch(
+        setNFTs({
+          list: opensea.data.assets,
+          page: 0,
+          status: 'idle'
+        })
+      );
+    } catch (error) {
+      dispatch(setStatus('error'));
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="flex mt-15 px-8 flex-wrap">
-        {nfts.length > 0 &&
-          nfts.map((nft) => {
+        <InfiniteScroll
+          dataLength={nfts.length} //This is important field to render the next data
+          next={handleLoadMore}
+          hasMore={(page + 1) * config.listLimit <= nfts.length}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          className="flex mt-15 px-8 flex-wrap"
+          refreshFunction={handleRefresh}
+          pullDownToRefresh
+          pullDownToRefreshThreshold={50}
+          pullDownToRefreshContent={
+            <div className="text-center py-5 w-full">Pull down to refresh</div>
+          }
+          releaseToRefreshContent={
+            <div className="text-center py-5 w-full">Release to refresh</div>
+          }
+        >
+          {nfts.map((nft) => {
             if (!nft.image_url) return null;
 
             if (nft.animation_url && nft.animation_url.includes('.mp4')) {
@@ -59,19 +104,9 @@ const NFTGallery = () => {
               />
             );
           })}
+        </InfiniteScroll>
       </div>
-      <>{status === 'loading' && <div>Fetching NFTs...</div>}</>
       <>{status === 'error' && <div>Something went wrong check console</div>}</>
-      <div className="text-center py-25">
-        {(page + 1) * config.listLimit <= nfts.length && (
-          <button
-            className="py-8 px-15 bg-white text-black rounded-m"
-            onClick={handleLoadMore}
-          >
-            Load more...
-          </button>
-        )}{' '}
-      </div>
     </>
   );
 };
